e592c60c00633556a0bcb2160a03692ccee9de62
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-03 11:20:35 +0545

    bubble short

---
+++ Shorthing/src/BubbleShort/Bubble.java
@@ -0,0 +1,24 @@
+package BubbleShort;
+
+public class Bubble {
+    public static void printArray(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,6,2,4,9,3,4};
+
+        for(int i = 0;i<arr.length-1;i++){
+            for(int j = 0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                     arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
