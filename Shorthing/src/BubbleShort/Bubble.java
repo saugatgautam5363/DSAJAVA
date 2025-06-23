f03e01ad649e14014324aae2f19ac9f527e2b965
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-23 22:18:27 +0545

    bubble short

--- Shorthing/src/BubbleShort/Bubble.java
+++ Shorthing/src/BubbleShort/Bubble.java
@@ -10,6 +10,7 @@ public class Bubble {
     public static void main(String[] args) {
         int arr[] = {1,6,2,4,9,3,4};
 
+
         for(int i = 0;i<arr.length-1;i++){
             for(int j = 0;j<arr.length-i-1;j++){
                 if(arr[j] > arr[j+1]){
